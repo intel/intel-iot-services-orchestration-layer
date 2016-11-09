bc1059b6770c3ed92b0adb5687b7d0a702546884
windmillknight <707442914@qq.com>
2016-11-09 20:00:25 +0800

    再次修改格式
    修改函数缩进。

--- center/lib/hubbundles/thingbundle/mpu9150/9_axis_attitude_fusion_algorithm/kernel.js
+++ center/lib/hubbundles/thingbundle/mpu9150/9_axis_attitude_fusion_algorithm/kernel.js
@@ -1,4 +1,4 @@
-
+​
 var sensor = new shared.sensorObj.MPU9150();
 sensor.init();
 
@@ -10,16 +10,11 @@ var angle_P=0,angle_R=0;
 var angleAx_P,gyroGy_P;
 var angleAx_R,gyroGy_R;
     
-function  ComplementaryFilter(ax,ay,az,gx,gy,gz,mx,my,mz,dt,K1){
-    
+function ComplementaryFilter(ax,ay,az,gx,gy,gz,mx,my,mz,dt,K1){
     angleAx_P = -Math.atan2(ax,az)*180/Math.PI;
-    
     gyroGy_P = gy;
-   
     angle_P = K1 * angleAx_P+ (1-K1) * (angle_P + gyroGy_P * dt);
-    
     pitch = angle_P + 5;
-    console.log(K1 * angleAx_P+ (1-K1) * (angle_P + gyroGy_P * dt));
     angleAx_R = Math.atan2(ay,az)*180/Math.PI;
     gyroGy_R = gx;
     angle_R = K1 * angleAx_R+ (1-K1) * (angle_R + gyroGy_R * dt);
@@ -32,31 +27,26 @@ function  ComplementaryFilter(ax,ay,az,gx,gy,gz,mx,my,mz,dt,K1){
       yaw:yaw,
       roll:roll
     });
-    console.log(pitch,yaw);
 }
 
- var Dt = parseFloat(IN.dt);
- var K = parseFloat(IN.K);
- if(IN.trigger===1){
+var Dt = parseFloat(CONFIG.dt);
+var K = parseFloat(CONFIG.K);
+if(IN.trigger===1){
+   sensor.update();
+   sensor.getAccelerometer(x, y, z);
+   var ax= shared.sensorObj.floatp_value(x);
+   var ay= shared.sensorObj.floatp_value(y);
+   var az= shared.sensorObj.floatp_value(z);
 
-    sensor.update();
+   sensor.getGyroscope(x, y, z);
+   var gx= shared.sensorObj.floatp_value(x);
+   var gy= shared.sensorObj.floatp_value(y);
+   var gz= shared.sensorObj.floatp_value(z);
 
-sensor.getAccelerometer(x, y, z);
-var ax= shared.sensorObj.floatp_value(x);
-var ay= shared.sensorObj.floatp_value(y);
-var az= shared.sensorObj.floatp_value(z);
-
-
-sensor.getGyroscope(x, y, z);
-var gx= shared.sensorObj.floatp_value(x);
-var gy= shared.sensorObj.floatp_value(y);
-var gz= shared.sensorObj.floatp_value(z);
-
-
-sensor.getMagnetometer(x, y, z);
-var mx= shared.sensorObj.floatp_value(x);
-var my= shared.sensorObj.floatp_value(y);
-var mz= shared.sensorObj.floatp_value(z)
-
-ComplementaryFilter(ax,ay,az,gx,gy,gz,mx,my,mz,Dt,K);
+   sensor.getMagnetometer(x, y, z);
+   var mx= shared.sensorObj.floatp_value(x);
+   var my= shared.sensorObj.floatp_value(y);
+   var mz= shared.sensorObj.floatp_value(z);
+ 
+   ComplementaryFilter(ax,ay,az,gx,gy,gz,mx,my,mz,Dt,K);
 }
